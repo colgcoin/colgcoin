d7b846fb69be70223abf833f75d9b876f3a4c629
colgcoin <colgcoin@gmail.com>
2019-01-21 17:45:48 +0000

    change P2P port

--- src/protocol.h
+++ src/protocol.h
@@ -19,7 +19,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 72396 : 72395;
+    return testnet ? 72396 : 47269;
 }
 
 
